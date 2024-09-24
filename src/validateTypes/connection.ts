bd3b9e68d4c0e95374d7e970bf633bff8d88c3ce
Katyara22 <106443049+Katyara22@users.noreply.github.com>
2024-09-24 15:52:08 +0200

    fix validations

--- src/validateTypes/connection.ts
+++ src/validateTypes/connection.ts
@@ -1,17 +1,17 @@
 import {
-    ELoginType, TConnectionMQTT,
+    TConnectionMQTT,
     TConnectionSettingsAzureConnectionString,
     TConnectionSettingsAzureOAuth,
 } from '../types';
 
 export const isTConnectionSettingsAzureOAuth = (obj: any): obj is TConnectionSettingsAzureOAuth => {
-    return obj && typeof obj === 'object' && obj.loginType === ELoginType.azureOAuth;
+    return obj && typeof obj === 'object' && 'subscription' in obj;
 };
 
 export const isTConnectionSettingsAzureConnectionString = (obj: any): obj is TConnectionSettingsAzureConnectionString => {
-    return obj && typeof obj === 'object' && obj.loginType === ELoginType.connectionString;
+    return obj && typeof obj === 'object' && 'connectionString' in obj;
 };
 
 export const isTConnectionMQTT = (obj: any): obj is TConnectionMQTT => {
-    return obj && typeof obj === 'object' && obj.loginType === ELoginType.mqtt;
+    return obj && typeof obj === 'object' && 'host' in obj;
 }
